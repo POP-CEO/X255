57bcfec48553af419d4e0b4f26f22d498a7237be
AbdElrahman_Mohamed <abdomohamed00001@gmail.com>
2017-07-15 09:46:35 +0200

    V 1
    delete file Test

--- Test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$url='https://www.google.com.eg/#safe=strict&q=f';
-// using file() function to get content
-$lines_array=file($url);
-// turn array into one variable
-$lines_string=implode('',$lines_array);
-//output, you can also save it locally on the server
-echo $lines_string;
